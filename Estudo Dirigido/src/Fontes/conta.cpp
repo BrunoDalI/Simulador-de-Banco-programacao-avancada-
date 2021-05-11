1ea856af15ae215f7a3fdfea117a0c245532819f
BrunoDall_ <83616760+BrunoDalI@users.noreply.github.com>
2021-05-10 23:06:02 -0300

    Update conta.cpp
    Modificação da documentação

--- Estudo Dirigido/src/Fontes/conta.cpp
+++ Estudo Dirigido/src/Fontes/conta.cpp
@@ -4,22 +4,23 @@
 
 
 
-// MÉTODOS GERAIS:
-// deposita(valor: float): void
+// MÃ‰TODOS GERAIS:
+// Realiza o deposito de uma quantia na conta;
 void Conta::deposita(float valor){
     //cout << "  Deposito de " << valor << "R$ efetuado.";
     _saldo += int(valor);
 }
 
 
-// retira(valor: float): void
+// Realiza uma retira de uma quantia da conta;
+void Conta::retira(float valor){
 void Conta::retira(float valor){
     //cout << "  Retirada de " << valor << "R$ realizada.";
     _saldo -= int(valor);
 }
 
 
-// transfere(ContaDestino: Conta): void
+// Tranfere um valor da conta A para a conta B
 void Conta::transfere(Conta *Destino, float valorTrasnf) {
     Destino->deposita(valorTrasnf);
     retira(valorTrasnf);
